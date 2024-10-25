8879fda5cd8122e3768fabdbff09f6c6c259c994
Ilan Wendling Thoele <ilanwendling@gmail.com>
2024-10-25 19:53:12 -0300

    Adicionar arquivos de projeto.

---
+++ Modela/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Modela.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
