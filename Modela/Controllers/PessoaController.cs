ddae538e23d4821ee9ccf762b243fed72d8bd5f5
Vinicius Tengaten <vinitengaten@gmail.com>
2024-10-26 19:55:06 -0300

    feat: início das views e controllers para gerenciamento de Pessoas

---
+++ Modela/Controllers/PessoaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Modela.Controllers;
+public class PessoaController : Controller {
+    public IActionResult Index() {
+        return View();
+    }
+
+    public IActionResult Edit(int id) {
+        return View("Index");
+    }
+
+    public IActionResult Create() {
+        return View("Index");
+    }
+
+    public IActionResult Update(int id) {
+        return View("Index");
+    }
+
+    public IActionResult Delete(int id) {
+        return View("Index");
+    }
+
+}
