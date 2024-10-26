6a01daff1ff93a50c3f20a7319f63b0a0c99bc99
Vinicius Tengaten <vinitengaten@gmail.com>
2024-10-26 19:47:22 -0300

    add: Pessoa e User model

---
+++ Modela/Models/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modela.Models;
+
+public class Pessoa {
+
+    public int Id { get; set; } = 0;
+
+    [Required]
+    public string? Nome { get; set; }
+
+    [Required]
+    public string? Sobrenome { get; set; }
+
+    public DateTime Nascimento { get; set; }
+
+    public char Sexo { get; set; } = ' ';
+
+    public Pessoa() { }
+
+    public Pessoa(int id, string? nome, string? sobrenome, DateTime nascimento, char sexo) {
+        Id = id;
+        Nome = nome;
+        Sobrenome = sobrenome;
+        Nascimento = nascimento;
+        Sexo = sexo;
+    }
+
+}
