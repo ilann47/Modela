09a6f1943999b7a3deb91bea938971ea9cb6ee80
mcakunz <matheus.kunz05@gmail.com>
2024-11-01 15:18:11 -0300

    [refactor] Refatorado o ILogger em HomeController de readonly para protected

--- Modela/Controllers/HomeController.cs
+++ Modela/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace Modela.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        protected readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
