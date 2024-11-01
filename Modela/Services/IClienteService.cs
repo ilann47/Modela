58b9281d9a74c8996f691250b85b8c2d137d018d
mcakunz <matheus.kunz05@gmail.com>
2024-11-01 15:15:29 -0300

    [feat] Adiciona a classe interface de servico IClienteService

---
+++ Modela/Services/IClienteService.cs
@@ -0,0 +1,14 @@
+﻿using Modela.Models;
+using Modela.DTOs;
+using System.Collections.Generic;
+
+namespace Modela.Services
+{
+    public interface IClienteService
+    {
+        Cliente Create(ClienteDTO clienteDTO);
+        Cliente GetById(int id);
+        IEnumerable<Cliente> GetAll();
+        Cliente Update(int id, ClienteDTO clienteDTO);
+    }
+}
